e57c79e2264cd94929f7f3533dc7d7a7983842de
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-05-02 13:12:27 -0700

    added search bar to sider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import Spotify from "./images/Spotify.png";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Album from "./pages/Album";
 import "./App.css";
 import { Layout } from "antd";
+import { SearchOutlined, DownCircleOutlined } from "@ant-design/icons";
 const { Header, Footer, Sider, Content } = Layout;
 
 
 const App = () => {
   return (
     <Layout>
-      <Sider width={300}>
+      <Sider width={300} className="sideBar">
         <img src={Spotify} alt="logo" className="logo"></img>
+        <div className="searchBar">
+          <span>Search</span>
+          <SearchOutlined style={{ fontSize: "30px" }}/>
+        </div>
       </Sider>
       <Content>
         <Routes>
