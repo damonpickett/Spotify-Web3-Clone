32042dad38e44bcae3e10c7c4f34cf07da73284d
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-05-02 12:01:26 -0700

    added ant design sider and content section app.js, added spotify logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import Spotify from "./images/Spotify.png";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Album from './pages/Album';
-import './App.css';
+import Album from "./pages/Album";
+import "./App.css";
+import { Layout } from "antd";
+const { Header, Footer, Sider, Content } = Layout;
+
 
 const App = () => {
-  return(
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/album" element={<Album />} />
-    </Routes>
-  )
+  return (
+    <Layout>
+      <Sider width={300}>
+        <img src={Spotify} alt="logo" className="logo"></img>
+      </Sider>
+      <Content>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/album" element={<Album />} />
+        </Routes>
+      </Content>
+    </Layout>
+  );
 };
 
 export default App;
