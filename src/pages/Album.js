38ffeb029df26debfa3653b694db10c0e79450a8
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-05-03 11:14:33 -0700

    completed album page, added play button and opensea button, added meta data for displaying album tracklist

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./Album.css";
 import { useLocation } from 'react-router-dom';
+import Opensea from "../images/opensea.png";
+import { ClockCircleOutlined } from "@ant-design/icons";
 
 const Album = () => {
   
@@ -95,6 +97,47 @@ const Album = () => {
         </div>
       </div>
     </div>
+    <div className='topBan'>
+      <div className='playButton' onClick={() => console.log("startMusic")}>
+        PLAY
+      </div>
+      <div
+        className='openButton'
+        onClick={() =>
+          window.open(
+            `https://testnets.opensea.io/assets/mumbai/${album.contract}/1`
+          )
+        }
+      >
+        OpenSea
+        <img src={Opensea} className='openLogo' alt='opensea' />
+      </div>
+    </div>
+    <div className='tableHeader'>
+      <div className='numberHeader'>#</div>
+      <div className='titleHeader'>TITLE</div>
+      <div className='numberHeader'>
+        <ClockCircleOutlined />
+      </div>
+    </div>
+    {albumDetails &&
+      albumDetails.map((nft, i) => {
+        nft = JSON.parse(nft.metadata);
+        return (
+          <>
+            <div className="tableContent">
+              <div className='numberHeader'>{i + 1}</div>
+              <div
+                className='titleHeader'
+                style={{ color: "rgb(205 203 203)" }}
+              >
+                {nft.name}
+              </div>
+              <div className='numberHeader'>{nft.duration}</div>
+            </div>
+          </>
+        );
+      })}
   </div>
   </>
 )
